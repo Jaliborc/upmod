f28c0e6077db46bbed9e14348cb04ae3018afa05
Joao Cardoso <jaliddons@gmail.com>
2026-02-08 21:54:28 +0000

    Now handles repositories automatically

--- cli.js
+++ cli.js
@@ -15,155 +15,153 @@ const path = require('path')
 const config = {}
 const savepath = path.join(require('os').homedir(), '.upmod')
 const settings = _.map([
-  {
-    name: 'dir',
-    message: 'WoW install directory',
-    filter: normalize,
-    validate: async dir => {
-      let s = await fs.stat(dir).catch(()=>{})
-      return s && s.isDirectory() || 'Not a valid directory path'
-    },
-  },
-  {
-    name: 'patches',
-    message: 'Supported game patch and toc number pairs',
-    filter: patches => _.map(patches.split(','), p => {
-      let pattern = p.trim().match(/^(\w+)[\/\\](\d+\.\d+\.\d+)[\/\\](\d+)$/)
-      return pattern && {flavor: pattern[1], name: pattern[2], toc: pattern[3]}
-    }),
-    transformer: patches => typeof(patches) != 'string' && _.every(patches) && _.map(patches, p => p.flavor + '/' + p.name + '/' + p.toc).join(', ') || patches,
-    validate: patches => _.every(patches) || 'Not in Flavor/X.X.X/TOC, Flavor/X.X.X/TOC format',
-  },
-  {
-    name: 'curse',
-    message: 'Curse login token',
-    validate: token => token.length > 5 || 'Too short for correct login token',
-  },
-  {
-    name: 'patrons',
-    message: 'Patron list .csv file',
-    filter: file => csv(fs.readFileSync(normalize(file), 'utf8'), {delimiter: ',', columns: true}),
-    transformer: patrons => typeof(patrons) != 'string' && _.has(patrons, 'length') && `${patrons.length} Patrons` || patrons,
-    validate: patrons => patrons.length > 0 || 'Not a valid .csv file',
-    optional: true,
-  },
+	{
+		name: 'dir',
+		message: 'WoW install directory',
+		filter: normalize,
+		validate: async dir => {
+			let s = await fs.stat(dir).catch(()=>{})
+			return s && s.isDirectory() || 'Not a valid directory path'
+		},
+	},
+	{
+		name: 'patches',
+		message: 'Supported game patch and toc number pairs',
+		filter: patches => _.map(patches.split(','), p => {
+			let pattern = p.trim().match(/^(\w+)[\/\\](\d+\.\d+\.\d+)[\/\\](\d+)$/)
+			return pattern && {flavor: pattern[1], name: pattern[2], toc: pattern[3]}
+		}),
+		transformer: patches => typeof(patches) != 'string' && _.every(patches) && _.map(patches, p => p.flavor + '/' + p.name + '/' + p.toc).join(', ') || patches,
+		validate: patches => _.every(patches) || 'Not in Flavor/X.X.X/TOC, Flavor/X.X.X/TOC format',
+	},
+	{
+		name: 'curse',
+		message: 'Curse login token',
+		validate: token => token.length > 5 || 'Too short for correct login token',
+	},
+	{
+		name: 'patrons',
+		message: 'Patron list .csv file',
+		filter: file => csv(fs.readFileSync(normalize(file), 'utf8'), {delimiter: ',', columns: true}),
+		transformer: patrons => typeof(patrons) != 'string' && _.has(patrons, 'length') && `${patrons.length} Patrons` || patrons,
+		validate: patrons => patrons.length > 0 || 'Not a valid .csv file',
+		optional: true,
+	},
 ], s => Object.assign({type: 'input', filter: v => v.trim(), transformer: v => v}, s))
 
 
 /* CLI */
 
 async function run() {
-  Object.assign(config, await fs.readJson(savepath).catch(() => {}))
-  await configure(set => !config[set.name] && !set.optional)
+	Object.assign(config, await fs.readJson(savepath).catch(() => {}))
+	await configure(set => !config[set.name] && !set.optional)
 
-  commander
-    .command('config')
-    .description('change global settings')
-    .action( () => {
-      configuration()
-    })
+	commander
+		.command('config')
+		.description('change global settings')
+		.action( () => {
+			configuration()
+		})
 
-  commander
-    .command('list')
-    .description('display found addons')
-    .action( () => {
-      for (let install of system.list(config.dir)) {
-        print(chalk`{bold {green ℹ} Under ${install.name}:}\n`)
+	commander
+		.command('list')
+		.description('display found addons')
+		.action( () => {
+			for (let install of system.list(config.dir)) {
+				print(chalk`{bold {green ℹ} Under ${install.name}:}\n`)
 
-        for (let mod of install.mods.sort())
-          print(chalk`❯ {cyan ${mod.name}}\n`)
-      }
-    })
+				for (let mod of install.mods.sort())
+					print(chalk`❯ {cyan ${mod.name}}\n`)
+			}
+		})
 
-  commander
-    .command('make <modname>')
-    .description('build .zip file in desktop')
-    .option('-p, --patch', 'name new patch build automatically')
-    .action( (mod, options) => {
-      let project = find(mod)
-      if (project)
-        system.make(Object.assign({changes: options.patch && autoPatch}, project, config))
-          .then(build => sucess(chalk`Built {cyan ${project.name}} version ${build.version}`))
-          .catch(error)
-    })
+	commander
+		.command('make <modname>')
+		.description('build .zip file in desktop')
+		.option('-l, --changelog <message>', 'add text into the changelog to name the new build')
+		.option('-s, --silent', 'do not update submodules before building')
+		.action( (mod, options) => {
+			let project = find(mod)
+			if (project)
+				system.make(Object.assign(options, project, config))
+					.then(build => sucess(chalk`Built {cyan ${project.name}} version ${build.version}`))
+					.catch(error)
+		})
 
-  commander
-    .command('up <modname>')
-    .description('build and upload given addon to curse')
-    .option('-p, --patch', 'name new patch build automatically')
-    .action( (mod, options) => {
-      let project = find(mod)
-      if (project)
-        system.make(Object.assign({changes: options.patch && autoPatch}, project, config))
-          .then(build => {
-            sucess(chalk`Built {cyan ${project.name}} version ${build.version}\n`)
-            system.upload(Object.assign(config, build))
-              .then(r => sucess(chalk`Uploaded {cyan ${project.name}} version ${build.version}`))
-              .catch(error)
-          })
-          .catch(error)
-    })
+	commander
+		.command('up <modname>')
+		.description('build and upload given addon to curse')
+		.option('-l, --changelog <message>', 'add text into the changelog to name the new build')
+		.option('-s, --silent', 'do not update submodules before building')
+		.action( (mod, options) => {
+			let project = find(mod)
+			if (project)
+				system.make(Object.assign(options, project, config))
+					.then(build => {
+						sucess(chalk`Built {cyan ${project.name}} version ${build.version}\n`)
+						system.upload(Object.assign(config, build))
+							.then(r => sucess(chalk`Uploaded {cyan ${project.name}} version ${build.version}`))
+							.catch(error)
+					})
+					.catch(error)
+		})
 
-  commander.version('2.0').parse(process.argv)
+	commander.version('2.0').parse(process.argv)
 }
 
 async function configuration() {
-  answer = await inquirer.prompt([{
-    name: 'setting',
-    type: 'list',
-    message: 'Choose setting to modify',
-    choices: _.map(settings,
-      s => ({value: s.name, name: chalk`${s.message} {yellow ─ ${s.transformer(config[s.name])}}`})
-    ),
-  }])
+	answer = await inquirer.prompt([{
+		name: 'setting',
+		type: 'list',
+		message: 'Choose setting to modify',
+		choices: _.map(settings,
+			s => ({value: s.name, name: chalk`${s.message} {yellow ─ ${s.transformer(config[s.name])}}`})
+		),
+	}])
 
-  await configure(s => s.name == answer.setting)
-  await configuration()
+	await configure(s => s.name == answer.setting)
+	await configuration()
 }
 
 async function configure(what) {
-  Object.assign(config, await inquirer.prompt(_.filter(settings, what)))
-  await save()
+	Object.assign(config, await inquirer.prompt(_.filter(settings, what)))
+	await save()
 }
 
 async function save() {
-  await fs.writeJson(savepath, config)
+	await fs.writeJson(savepath, config)
 }
 
 
 /* Util */
 
 function find(mod) {
-  let mods = _.flatten(_.map(system.list(config.dir), i => i.mods))
-  let match = _.find(mods, entry => clean(entry.name) == clean(mod))
-  if (!match)
-    error(chalk`{cyan ${mod}} is not registered.`)
+	let mods = _.flatten(_.map(system.list(config.dir), i => i.mods))
+	let match = _.find(mods, entry => clean(entry.name) == clean(mod))
+	if (!match)
+		error(chalk`{cyan ${mod}} is not registered.`)
 
-  return match
+	return match
 }
 
 function clean(mod) {
-  return mod.replace('-', '').replace('_', '').toLowerCase()
+	return mod.replace('-', '').replace('_', '').toLowerCase()
 }
 
 function normalize(file) {
-  return path.normalize(file.trim().replace(/"/g, ''))
-}
-
-function autoPatch(txt) {
-  return `##### ${config.patches[0].name}\n* Updated for World of Warcraft patch ${config.patches[0].name}.\n\n` + txt
+	return path.normalize(file.trim().replace(/"/g, ''))
 }
 
 function sucess(txt) {
-  print(chalk`{green ✔} ${txt}`)
+	print(chalk`{green ✔} ${txt}`)
 }
 
 function error(txt) {
-  print(chalk`{red ❯❯} ${txt}`)
+	print(chalk`{red ❯❯} ${txt}`)
 }
 
 function print(text) {
-  process.stdout.write(figures(text))
+	process.stdout.write(figures(text))
 }
 
 run()
--- index.js
+++ index.js
@@ -1,173 +1,231 @@
 const process = require('process')
 process.removeAllListeners('warning')
 
+const { execSync, spawnSync } = require('child_process')
+const archiver = require('archiver')
+const klaw = require('klaw-sync')
+
 const _ = require('lodash')
 const b = require('bluebird')
 const chalk = require('chalk')
 
 const fsreplace = require('replace-in-file')
 const request = require('request-promise')
-const archiver = require('archiver')
-const klaw = require('klaw-sync')
 const fs = require('fs-extra')
 const path = require('path')
 const os = require('os')
 
+
+// Public
 function list(dir) {
-  return _
-    .chain(fs.readdirSync(dir, {withFileTypes: true}))
-    .each(i => i.path = path.join(dir, i.name, 'Interface', 'Addons'))
-    .filter(i => i.name.match(/^_.+_$/) && check(i.path))
-    .each(i => i.mods = _
-      .chain(fs.readdirSync(i.path))
-      .filter(mod => check(path.join(i.path, mod, '.upconfig')))
-      .map(mod => ({name: mod, path: path.join(i.path, mod)}))
-      .value()
-    )
-    .value()
+	return _
+		.chain(fs.readdirSync(dir, {withFileTypes: true}))
+		.each(i => i.path = path.join(dir, i.name, 'Interface', 'Addons'))
+			.filter(i => i.name.match(/^_.+_$/) && exists(i.path))
+			.each(i => i.mods = _
+			.chain(fs.readdirSync(i.path))
+			.filter(mod => exists(path.join(i.path, mod, '.upconfig')))
+			.map(mod => ({name: mod, path: path.join(i.path, mod)}))
+			.value())
+		.value()
 }
 
 async function make(params) {
-  let upconfig = readconfig(path.join(params.path, '.upconfig'))
-  let folders = _.concat(_.map(upconfig.modules || [], m => path.join(params.path, '../', m)), params.path)
-  for (var folder of folders)
-    if (!check(folder))
-      throw chalk`Missing a required module at {red ${folder}}`
+	let upconfig = readconfig(path.join(params.path, '.upconfig'))
+	if (!upconfig)
+		throw chalk`No {red .upconfig} found`
 
-  let logfile = path.join(params.path, 'Changelog.md')
-  if (!check(logfile))
-    throw chalk`No {red Changelog.md} found`
+	let folders = _.concat(_.map(upconfig.modules || [], m => path.join(params.path, '../', m)), params.path)
+	for (var folder of folders)
+		if (!exists(folder))
+			throw chalk`Missing a required module at {red ${folder}}`
 
-  let log = (params.changes || (l => l))(read(logfile))
-  let name = log.match(/^[^\n\r\d]*(\d+(?:\.\d+)?(?:\.\d+)?)(?:\s*\((beta)\))?/)
-  if (!name)
-    throw chalk`Invalid {red Changelog.md} format`
+	if (!params.stay)
+		updateSubmodules(folders)
 
-  let year = (new Date()).getFullYear()
-  let version = name[1], type = name[2] || 'release'
-  let patrons = _
-    .chain(params.patrons || []).each(parsePatron)
-    .filter(p => p['Patron Status'] == 'Active patron' && p.Tier && p.Tier != 'Mankrik\'s Wife' && p.Lifetime > 0 && p.Pledge >= 5)
-    .sortBy('Lifetime').reverse()
-    .groupBy('Tier').toPairs()
-    .sortBy(tier => _.meanBy(tier[1], 'Pledge')).reverse()
-    .reduce((t, tier) => t + `{title='${tier[0]}',people={` + _.reduce(tier[1], (t, p) => t + `'${capitalize(p.Name)}',`, '').slice(0,-1) + '}},', '')
-    .value().slice(0,-1)
+	let logfile = path.join(params.path, 'Changelog.md')
+	if (!exists(logfile))
+		throw chalk`No {red Changelog.md} found`
 
-  let incompatible = upconfig.incompatible?.filter(v => v.length > 0).map(v => `^${v.replace(/x/g, '\\d+')}$`) || []
-  let patches = params.patches.filter(p => !_.some(incompatible, i => p.name.match(i)))
+	let changes = params.changelog?.replace(/\\|\/|\/n/g, '\n')
+	let log = changes ? `${changes}\n\n${read(logfile)}` : read(logfile)
+	if (changes)
+		await fs.writeFile(logfile, log, 'utf8')
 
-  let files = _.flatMap(folders, folder => _.map(klaw(folder), i => i.path)).filter(file => !/\\\./g.test(file))
-  let dir = path.join(params.path, '../')
+	let title = log.match(/^[^\n\r\d]*(\d+(?:\.\d+)?(?:\.\d+)?)(?:\s*\((beta)\))?/)
+	if (!title)
+		throw chalk`Invalid {red Changelog.md} format`
 
-  await fs.writeFile(logfile, log, 'utf8')
-  await b.each(files, async file => {
-    if (file.endsWith('.lua')) {
-      await fsreplace({files: file, from: /(local\s+\S+\s*=\s*)[^\n\r]+(\-\-\s*generated\s*patron\s*list)/g, to: `$1{${patrons}} $2`})
-      await fsreplace({files: file, from: /(Copyright[^\n\r\t\d]+\d+\s*\-\s*)\d+/g, to: `$1${year}`})
-    } else if (file.endsWith('.toc')) {
-      let matches = patches.filter(patch => file.slice(0, -4).toLowerCase().endsWith(patch.flavor.toLowerCase()))
-      matches = matches.length > 0 ? matches : patches
+	let year = (new Date()).getFullYear()
+	let version = title[1], type = title[2] || 'release'
+	let patrons = _
+		.chain(params.patrons || []).each(parsePatron)
+		.filter(p => p['Patron Status'] == 'Active patron' && p.Tier && p.Tier != 'Mankrik\'s Wife' && p.Lifetime > 0 && p.Pledge >= 5)
+		.sortBy('Lifetime').reverse()
+		.groupBy('Tier').toPairs()
+		.sortBy(tier => _.meanBy(tier[1], 'Pledge')).reverse()
+		.reduce((t, tier) => t + `{title='${tier[0]}',people={` + _.reduce(tier[1], (t, p) => t + `'${capitalize(p.Name)}',`, '').slice(0,-1) + '}},', '')
+		.value().slice(0,-1)
 
-      await fsreplace({files: file, from: /(##\s*Interface:)\s*([^\n\r\t]+)/, to: `$1 ${_.map(matches, 'toc').join(', ')}`})
-      await fsreplace({files: file, from: /(##\s*Version:\s*).*$/m, to: `$1${version}${type !== 'release' ? ' ('+type+')' : ''}`})
-    }
-  })
+	let incompatible = upconfig.incompatible?.filter(v => v.length > 0).map(v => `^${v.replace(/x/g, '\\d+')}$`) || []
+	let compatible = params.patches.filter(p => !_.some(incompatible, i => p.name.match(i)))
 
-  let zip = archiver('zip')
-  let out = path.join(os.homedir(), 'Desktop', `${params.name}-${version}.zip`)
-  let ignore = require('ignore')().add(upconfig.ignore && upconfig.ignore.join('\n'))
+	let files = _.flatMap(folders, folder => _.map(klaw(folder), i => i.path)).filter(file => !/\\\./g.test(file))
+	let dir = path.join(params.path, '../')
 
-  await zip.pipe(fs.createWriteStream(out))
-  await b.each(files, async file => {
-    let ext = path.extname(file)
-    let fout = {name: path.relative(dir, file)}
-    let ignored = ignore.ignores(path.relative(dir, file))
+	await fs.writeFile(logfile, log, 'utf8')
+	await b.each(files, async file => {
+		if (file.endsWith('.lua')) {
+			await fsreplace({files: file, from: /(local\s+\S+\s*=\s*)[^\n\r]+(\-\-\s*generated\s*patron\s*list)/g, to: `$1{${patrons}} $2`})
+			await fsreplace({files: file, from: /(Copyright[^\n\r\t\d]+\d+\s*\-\s*)\d+/g, to: `$1${year}`})
+		} else if (file.endsWith('.toc')) {
+			let isRoot = folders.includes(path.dirname(file))
+			let interfaces = isRoot ? compatible : params.patches
 
-    if (ext == '.lua') {
-      zip.append(ignored && 'if true then return end' || fs.createReadStream(file), fout)
-    } else if (ext == '.xml') {
-      zip.append(ignored && '<Ui></Ui>' || fs.createReadStream(file), fout)
-    } else if (ext == '.tga' || ext == '.mp3') {
-      if (!ignored) zip.append(fs.createReadStream(file), fout)
-    } else if (ext == '.toc') {
-      if (!ignored) {
-        let toc = fs.readFileSync(file, 'utf8').replace(/(##\s*Title:\s*)\|c\w{8}(.+)\|r\s*(\r\n?|\n)/, '$1$2$3')
-        if (type == 'release')
-          toc = toc.replace(/^## X-Development:.*(\r?\n|\r)/gm, '')
+			let matches = interfaces.filter(patch => file.slice(0, -4).toLowerCase().endsWith(patch.flavor.toLowerCase()))
+			matches = matches.length > 0 ? matches : interfaces
 
-        zip.append(toc, fout)
-      }
-    }
-  }).then(() => zip.finalize())
+			await fsreplace({files: file, from: /(##\s*Interface:)\s*([^\n\r\t]+)/, to: `$1 ${_.map(matches, 'toc').join(', ')}`})
+			await fsreplace({files: file, from: /(##\s*Version:\s*).*$/m, to: `$1${version}${type !== 'release' ? ' ('+type+')' : ''}`})
+		}
+	})
 
-  let id = upconfig.project && _.find(upconfig.project)
-  return {project: id, version: version, patches: patches, type: type, log: log, path: out}
-}
+	if (!params.silent) {
+		commitSubmodules(folders, 'Automated year/interface number update')
+		commitChanges(params.path, version)
+	}
 
-function readconfig(file) {
-  let text = read(file)
-  let data = {}
+	let zip = archiver('zip')
+	let out = path.join(os.homedir(), 'Desktop', `${params.name}-${version}.zip`)
+	let ignore = require('ignore')().add(upconfig.ignore && upconfig.ignore.join('\n'))
 
-  if (text) {
-    let key
+	await zip.pipe(fs.createWriteStream(out))
+	await b.each(files, async file => {
+		let ext = path.extname(file)
+		let fout = {name: path.relative(dir, file)}
+		let ignored = ignore.ignores(path.relative(dir, file))
 
-    for (let line of text.split('\n')) {
-      let v = line.match(/^\s*\[(\w+)\]\s*$/)
-      if (v) {
-        key = v[1]
-        data[key] = []
-      } else if (key) {
-        data[key].push(line.trim())
-      }
-    }
-  }
+		if (ext == '.lua') {
+			zip.append(ignored ? 'if true then return end' : fs.createReadStream(file), fout)
+		} else if (ext == '.xml') {
+			zip.append(ignored ? '<Ui></Ui>' : fs.createReadStream(file), fout)
+		} else if (ext == '.tga' || ext == '.mp3') {
+			if (!ignored) zip.append(fs.createReadStream(file), fout)
+		} else if (ext == '.toc') {
+			if (!ignored && folders.includes(path.dirname(file)))
+				zip.append(fs.readFileSync(file, 'utf8').replace(/(##\s*Title:\s*)\|c\w{8}(.+)\|r\s*(\r\n?|\n)/, '$1$2$3'), fout)
+		}
+	}).then(() => zip.finalize())
 
-  return data
-}
-
-function check(file) {
-  return fs.existsSync(file) && file
-}
-
-function read(file) {
-  return fs.existsSync(file) && fs.readFileSync(file, 'utf8')
-}
-
-function capitalize(text) {
-  return text.split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1).toLowerCase()).join(' ').substring(0, 17).replace(/'/g, "\\'")
-}
-
-function parsePatron(entry, key) {
-  parseDollars(entry, 'Pledge')
-  parseDollars(entry, 'Lifetime')
-}
-
-function parseDollars(entry, key) {
-  entry[key] = parseInt(entry[key + ' Amount'].match(/\d+/)[0])
+	let id = upconfig.project && _.find(upconfig.project)
+	return {project: id, version: version, patches: compatible, type: type, log: log, path: out}
 }
 
 async function upload(params) {
-  let headers =  {'User-Agent': 'UpMod/2.0.0', 'X-Api-Token': params.curse}
-  let clients = await request.get({url: 'https://wow.curseforge.com/api/game/versions', headers: headers, json: true})
-  let compatible = _.filter(clients, c => _.some(params.patches || [], p => p.name == c.name))
-  if (compatible.length < params.patches.length)
-    throw chalk`Only ${compatible.length} compatible WoW patches found`
+	let headers =  {'User-Agent': 'UpMod/2.0.0', 'X-Api-Token': params.curse}
+	let clients = await request.get({url: 'https://wow.curseforge.com/api/game/versions', headers: headers, json: true})
+	let compatible = _.filter(clients, c => _.some(params.patches || [], p => p.name == c.name))
+	if (compatible.length < params.patches.length)
+		throw chalk`Only ${compatible.length} compatible WoW patches found`
 
-  return await request.post({
-    url:`https://wow.curseforge.com/api/projects/${params.project}/upload-file`,
-    headers: headers,
-    formData: {
-      file: fs.createReadStream(params.path),
-      metadata : JSON.stringify({
-        gameVersions: _.map(compatible, 'id'),
-        displayName: params.version,
-        releaseType: params.type,
-        changelog: params.log,
-        changelogType: 'markdown',
-      })
-    }
-  })
+	return await request.post({
+		url:`https://wow.curseforge.com/api/projects/${params.project}/upload-file`,
+		headers: headers,
+		formData: {
+		file: fs.createReadStream(params.path),
+		metadata : JSON.stringify({
+			gameVersions: _.map(compatible, 'id'),
+			displayName: params.version,
+			releaseType: params.type,
+			changelog: params.log,
+			changelogType: 'markdown',
+		})
+		}
+	})
+}
+
+
+// Git
+function updateSubmodules(folders) {
+	for (let folder of folders) {
+		if (exists(path.join(folder, '.git'))) {
+			let pull = spawnSync('git', ['pull'], {cwd: folder, stdio: 'inherit'})
+			if (pull.status !== 0)
+				throw chalk`Failed to pull {red ${folder}} from remote`
+
+			let update = spawnSync('git', ['submodule', 'update', '--remote', '--recursive'], {cwd: folder, stdio: 'inherit'})
+			if (update.status !== 0)
+				throw chalk`Failed to update submodule in {red ${folder}}`
+		}
+	}
+}
+
+function commitSubmodules(folders, message) {
+	for (let folder of folders)
+		if (exists(path.join(folder, '.git')))
+			for (let repo of getSubmodules(folder))
+				commitChanges(repo, message)
+}
+
+function commitChanges(repo, message) {
+	let status = spawnSync('git', ['status', '--porcelain'], {cwd: repo, encoding: 'utf8'})
+	if (status?.stdout?.trim().length > 0) {
+		execSync('git', ['add', '.'], {cwd: repo})
+		execSync('git', ['commit', '-m', message], {cwd: repo, stdio: 'inherit'})
+		execSync('git', ['push', 'origin'], {cwd: repo, stdio: 'inherit'})
+	}
+}
+
+function getSubmodules(folder) {
+	let modules = spawnSync('git', ['submodule', '--quiet', 'foreach', 'echo $displaypath'], {cwd: folder, encoding: 'utf8'})
+	if (modules.status !== 0)
+		throw chalk`Failed to find submodules in {red ${folder}}`
+
+	return modules.stdout.split('\n').map(s => s.trim()).filter(Boolean).map(s => path.join(folder, s))
+}
+
+
+// Utils
+function readconfig(path) {
+	let text = read(path)
+	let data = {}
+
+	if (text) {
+		let key
+
+		for (let line of text.split('\n')) {
+		let v = line.match(/^\s*\[(\w+)\]\s*$/)
+		if (v) {
+			key = v[1]
+			data[key] = []
+		} else if (key) {
+			data[key].push(line.trim())
+		}
+		}
+	}
+
+	return data
+}
+
+function exists(file) {
+	return fs.existsSync(file) && file
+}
+
+function read(file) {
+	return fs.existsSync(file) && fs.readFileSync(file, 'utf8')
+}
+
+function capitalize(text) {
+	return text.split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1).toLowerCase()).join(' ').substring(0, 17).replace(/'/g, "\\'")
+}
+
+function parsePatron(entry) {
+	parseDollars(entry, 'Pledge')
+	parseDollars(entry, 'Lifetime')
+}
+
+function parseDollars(entry, key) {
+	entry[key] = parseInt(entry[key + ' Amount'].match(/\d+/)[0])
 }
 
 module.exports = {list: list, make: make, upload: upload}
